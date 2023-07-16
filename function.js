5f5afeaa8b41dbd9350dad74ba80d8dfc9e6fec1
softenglj <124595944+softenglj@users.noreply.github.com>
2023-07-16 19:11:51 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,14 @@
+alert("Hi");
+alert("Happy 18th birthday");
+alert("Bago mo matapos tong alert na to");
+alert("Gusto ko lng sabihin na");
+alert("Ang saya ko para sayo^;^");
+alert("At kung gusto mo may music habang binabasa at scroll ka dito");
+alert("Pindutin mo lng yung button na nasa gilid at mag play na to");
+alert("Salamat sana maappreciate hehe");
+
+var x = document.getElementById("enc");
+
+function playAudio() {
+    x.play();
+}
